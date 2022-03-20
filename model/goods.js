b3232784911c03e47400a04a2c42db195bb8395b
F999999999 <9811@88.com>
2022-03-20 18:17:03 +0800

    新增：查询商品日志时返回更多数据

--- model/goods.js
+++ model/goods.js
@@ -1,10 +1,10 @@
 const { query } = require("../utils/mysql");
 
 // 获取商品信息
-module.exports.getGoods = async ({ store_id, state = 1 }) => {
+module.exports.getGoods = async ({ store_id, states = 1 }) => {
   return await query(
     `SELECT id,name,weight,shelflife,production_date,storage_time,store_id,shelf_id,shelf_grid_id FROM store_goods WHERE store_id = ? AND shelf_id > 0 AND states = ?`,
-    [store_id, state]
+    [store_id, states]
   );
 };
 
@@ -30,7 +30,7 @@ module.exports.addGoods = async ({
   storage_time = -1,
   shelf_id,
   shelf_grid_id,
-  state = 1,
+  states = 1,
 }) => {
   return await query(
     `INSERT INTO store_goods (store_id,name,weight,shelflife,production_date,storage_time,shelf_id,shelf_grid_id,states) VALUES (?,?,?,?,?,?,?,?,?)`,
@@ -43,7 +43,7 @@ module.exports.addGoods = async ({
       storage_time,
       shelf_id,
       shelf_grid_id,
-      state,
+      states,
     ]
   );
 };
@@ -81,13 +81,13 @@ module.exports.addGoodsLog = async ({
 };
 
 // 获取商品位置信息
-module.exports.getGoodsPosition = async ({ ids, state = 1 }) => {
-  const sql = ids.reduce(
-    (prev, curr, i, arr) =>
-      prev + `id = ${curr}` + (i === arr.length - 1 ? ";" : " OR "),
-    `SELECT *,store_id,shelf_id,shelf_grid_id FROM store_goods WHERE states = ? AND `
-  );
-  return await query(sql, [state]);
+module.exports.getGoodsPosition = async ({ ids, states = 1 }) => {
+  const payload = [states];
+  const sql = ids.reduce((prev, curr, i, arr) => {
+    payload.push(curr);
+    return prev + "id = ?" + (i === arr.length - 1 ? ";" : " OR ");
+  }, `SELECT *,store_id,shelf_id,shelf_grid_id FROM store_goods WHERE states = ? AND `);
+  return await query(sql, payload);
 };
 
 // 修改商品位置信息
@@ -104,33 +104,61 @@ module.exports.updateGoodsPosition = async ({
 };
 
 // 修改商品状态
-module.exports.updateGoodsState = async ({ ids, states }) => {
-  const sql = ids.reduce(
-    (prev, curr, i, arr) =>
-      prev + `id = ${curr}` + (i === arr.length - 1 ? ";" : " OR "),
-    `UPDATE store_goods SET states = ? WHERE `
-  );
-  return await query(sql, [states]);
+module.exports.updateGoodsState = async ({ ids, states = 1 }) => {
+  const payload = [states];
+  const sql = ids.reduce((prev, curr, i, arr) => {
+    payload.push(curr);
+    return prev + "id = ?" + (i === arr.length - 1 ? ";" : " OR ");
+  }, `UPDATE store_goods SET states = ? WHERE `);
+  return await query(sql, payload);
 };
 
 // 模糊搜索商品
 module.exports.fuzzySearchGoodsByName = async ({
   store_id,
   name,
-  state = 1,
+  states = 1,
 }) => {
   return await query(
     `SELECT id,name,weight,shelflife,production_date,storage_time,shelf_id,shelf_grid_id FROM store_goods WHERE ${
       store_id ? "store_id = ? AND " : ""
     }states = ? AND name LIKE '%${name}%'`,
-    store_id ? [store_id, state] : [state]
+    store_id ? [store_id, states] : [states]
   );
 };
 
 // 查询商品日志
 module.exports.getGoodsLog = async ({ page_num = 1, page_size = 10 }) => {
   return await query(
-    `SELECT id,goods_id,(SELECT name FROM store_goods WHERE id = goods_id) AS goods_name,before_store_id,before_shelf_id,before_shelf_grid_id,now_store_id,now_shelf_id,now_shelf_grid_id,storage_time,takeout_time,operate_id,(SELECT username FROM sys_user WHERE id = operate_id) AS operate_name,operate_time FROM store_goods_log order by operate_time desc LIMIT ?,?`,
+    `
+SELECT 
+id,
+goods_id,
+(SELECT name FROM store_goods WHERE id = goods_id) AS goods_name,
+before_store_id,
+CASE WHEN before_store_id > 0 THEN (SELECT name FROM store WHERE id = before_store_id) ELSE null END AS before_store_name,
+before_shelf_id,
+CASE WHEN before_shelf_id > 0 THEN (SELECT name FROM store_shelf WHERE id = before_shelf_id) ELSE null END AS before_shelf_name,
+before_shelf_grid_id,
+CASE WHEN before_shelf_grid_id > 0 THEN (SELECT x FROM store_shelf_grid WHERE store_shelf_grid.goods_id = store_goods_log.goods_id) ELSE null END AS before_shelf_grid_x,
+CASE WHEN before_shelf_grid_id > 0 THEN (SELECT y FROM store_shelf_grid WHERE store_shelf_grid.goods_id = store_goods_log.goods_id) ELSE null END AS before_shelf_grid_y,
+CASE WHEN before_shelf_grid_id > 0 THEN (SELECT z FROM store_shelf_grid WHERE store_shelf_grid.goods_id = store_goods_log.goods_id) ELSE null END AS before_shelf_grid_z,
+now_store_id,
+CASE WHEN now_store_id > 0 THEN (SELECT name FROM store WHERE id = now_store_id) ELSE null END AS now_store_name,
+now_shelf_id,
+CASE WHEN now_shelf_id > 0 THEN (SELECT name FROM store_shelf WHERE id = now_shelf_id) ELSE null END AS now_shelf_name,
+now_shelf_grid_id,
+CASE WHEN now_shelf_grid_id > 0 THEN (SELECT x FROM store_shelf_grid WHERE store_shelf_grid.goods_id = store_goods_log.goods_id) ELSE null END AS now_shelf_grid_x,
+CASE WHEN now_shelf_grid_id > 0 THEN (SELECT y FROM store_shelf_grid WHERE store_shelf_grid.goods_id = store_goods_log.goods_id) ELSE null END AS now_shelf_grid_y,
+CASE WHEN now_shelf_grid_id > 0 THEN (SELECT z FROM store_shelf_grid WHERE store_shelf_grid.goods_id = store_goods_log.goods_id) ELSE null END AS now_shelf_grid_z,
+storage_time,
+takeout_time,
+operate_id,
+(SELECT username FROM sys_user WHERE id = operate_id) AS operate_name,operate_time 
+FROM store_goods_log 
+order by operate_time desc 
+LIMIT ?,?
+`,
     [(page_num - 1) * page_size, page_size]
   );
 };
